c5feca5963b5c542186c06b178e040de7ec587d0
Carol Holmstedt <cjkulzer@gmail.com>
2024-06-24 07:42:01 -0700

    Task 8 Completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,8 +5,8 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+    //write code to check is the username is valid
 }
 
 // Check if the user with the given username and password exists
@@ -24,7 +24,7 @@ const authenticatedUser = (username, password) => {
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
@@ -57,20 +57,27 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     let review = req.query.review;
     let numberOfReviews = Object.keys(book.reviews).length;
     let newEntry = numberOfReviews + 1;
+    let userFound = false;
+
     // loop through the reviews to see if the user has already made a review
     Object.keys(book.reviews).forEach((entry) => {
-      if (entry.user === loggedInUser) {
-          // replace the existing review with the current review
-          entry.text = review;
-          return res.status(200).json({message: "Review updated"});
-      }
-    })
+        if (book.reviews[parseInt(entry)].user === loggedInUser) {
+            // replace the existing review with the current review
+            book.reviews[parseInt(entry)].text = review;
+            userFound = true;
+            return res.status(200).json({ message: "Review updated" });
+        }
+    });
+
     // if the user was not found, then this is a new entry to add
-    let newReview = {"user": loggedInUser, "text": review};
-    Object.assign(book.reviews, {newEntry: newReview});
-  
-    return res.status(200).json({message: "Review added"});
-  });
+    if (!userFound) {
+        let newReview = { "user": loggedInUser, "text": review };
+        Object.assign(book.reviews, { [newEntry]: newReview });
+
+        return res.status(200).json({ message: "Review added" });
+    }
+
+});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
